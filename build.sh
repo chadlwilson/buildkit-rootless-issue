e22681380c92e73cfd6642cbc395301f6ff5fbf1
Chad Wilson <chadw@thoughtworks.com>
2024-11-04 11:16:20 +0800

    More minimal

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ for buildkit in "v0.16.0" "v0.17.0"; do
   docker buildx rm --force tmp-builder || true
   docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$buildkit-rootless
   docker buildx inspect --bootstrap tmp-builder
-  docker buildx build --pull --platform linux/amd64,linux/arm64 --output type=oci,dest=test.tar . --tag latest
+  docker buildx build --pull --platform linux/amd64,linux/arm64 . --tag latest
 done
