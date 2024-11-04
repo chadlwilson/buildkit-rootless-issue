03e12f6284d022befaa42636c2c4ba4313e66544
Chad Wilson <chadw@thoughtworks.com>
2024-11-04 23:55:22 +0800

    Try matrix build

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ set -ex
 
 docker info
 
-for buildkit in "v0.16.0" "v0.17.0"; do
-  echo "Building with $buildkit"
-  docker buildx rm --force tmp-builder || true
-  docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$buildkit-rootless
-  docker buildx inspect --bootstrap tmp-builder
-  docker buildx build --pull --platform linux/amd64 . --tag latest
-done
+echo "Building with $BUILDKIT_VERSION"
+docker buildx rm --force tmp-builder || true
+docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$BUILDKIT_VERSION-rootless
+docker buildx inspect --bootstrap tmp-builder
+docker buildx build --pull --platform linux/amd64 . --tag latest
