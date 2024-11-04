dd49a28d8da9c9c5099e376e27138543ea599cbd
Chad Wilson <chadw@thoughtworks.com>
2024-11-04 11:08:52 +0800

    Try and make minimal

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 for buildkit in "v0.16.0" "v0.17.0"; do
   echo "Building with $buildkit"
-  docker buildx rm --force --keep-state tmp-builder || true
+  docker buildx rm --force tmp-builder || true
   docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$buildkit-rootless
   docker buildx inspect --bootstrap tmp-builder
   docker buildx build --pull --platform linux/amd64,linux/arm64 --output type=oci,dest=test.tar . --tag latest
