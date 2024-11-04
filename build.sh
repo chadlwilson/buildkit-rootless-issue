6705465c67814bc5f0aa78c9810f374346b87feb
Chad Wilson <chadw@thoughtworks.com>
2024-11-04 11:00:08 +0800

    Build with both versions

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-docker buildx rm --force --keep-state tmp-builder || true
-docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:buildx-stable-1-rootless
-docker buildx inspect --bootstrap tmp-builder
-docker buildx build --pull --platform linux/amd64,linux/arm64 --output type=oci,dest=test.tar . --tag latest
+
+for buildkit in "v0.16.0" "v0.17.0"; do
+  echo "Building with $buildkit"
+  docker buildx rm --force --keep-state tmp-builder || true
+  docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:v$buildkit-rootless
+  docker buildx inspect --bootstrap tmp-builder
+  docker buildx build --pull --platform linux/amd64,linux/arm64 --output type=oci,dest=test.tar . --tag latest
+done
