536ce78643000a50829242e393a9233bb87f4900
Chad Wilson <chadw@thoughtworks.com>
2024-11-09 23:02:30 +0800

    Remove fixed target platform

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -ex
 
 docker info
 
-echo "Building with $BUILDKIT_VERSION"
+echo "Building with buildkit=$BUILDKIT_VERSION"
 docker buildx rm --force tmp-builder || true
 docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$BUILDKIT_VERSION-rootless
 docker buildx inspect --bootstrap tmp-builder
-docker buildx build --pull --platform linux/amd64 . --tag latest
+docker buildx build --pull . --tag latest
